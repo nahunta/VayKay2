a1a91c6626e8a6bf1038babcbbedefc1f43fa642
Nelson Taylor <nelson.taylor@Nelson-Taylor.local>
2015-07-31 16:34:56 -0500

    Initial Commit

---
+++ VayKay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VayKay
+//
+//  Created by Nelson Taylor on 7/31/15.
+//  Copyright (c) 2015 Nelson Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
